e7cc3ead1a2803c7a6d270e872d6b983ba18dcb8
HauLTCE <letrunghau2244@gmail.com>
2025-09-29 22:17:22 +0700

    yes

--- Unleashed/Unleashed.AppHost/AppHost.cs
+++ Unleashed/Unleashed.AppHost/AppHost.cs
@@ -3,13 +3,54 @@ var builder = DistributedApplication.CreateBuilder(args);
 //===========================================================================================================================================
 
 var authService = builder.AddProject<Projects.AuthService>("authservice");
+
 var productService = builder.AddProject<Projects.ProductService>("productservice");
+
 var inventoryService = builder.AddProject<Projects.InventoryService>("inventoryservice");
+
 var cartService = builder.AddProject<Projects.CartService>("cartservice");
+
 var orderService = builder.AddProject<Projects.OrderService>("orderservice");
+
 var discountService = builder.AddProject<Projects.DiscountService>("discountservice");
+
 var reviewService = builder.AddProject<Projects.ReviewService>("reviewservice");
+
 var notificationService = builder.AddProject<Projects.NotificationService>("notificationservice");
+
+//===========================================================================================================================================//===========================================================================================================================================
+
+//var authService = builder
+//    .AddProject<Projects.AuthService>("authservice")
+//    .WithHttpEndpoint(port: 10001, name: "http-auth");
+
+//var productService = builder
+//    .AddProject<Projects.ProductService>("productservice")
+//    .WithHttpEndpoint(port: 10002, name: "http-product");
+
+//var inventoryService = builder
+//    .AddProject<Projects.InventoryService>("inventoryservice")
+//    .WithHttpEndpoint(port: 10003, name: "http-inventory");
+
+//var cartService = builder
+//    .AddProject<Projects.CartService>("cartservice")
+//    .WithHttpEndpoint(port: 10004, name: "http-cart");
+
+//var orderService = builder
+//    .AddProject<Projects.OrderService>("orderservice")
+//    .WithHttpEndpoint(port: 10005, name: "http-order");
+
+//var discountService = builder
+//    .AddProject<Projects.DiscountService>("discountservice")
+//    .WithHttpEndpoint(port: 10006, name: "http-discount");
+
+//var reviewService = builder
+//    .AddProject<Projects.ReviewService>("reviewservice")
+//    .WithHttpEndpoint(port: 10007, name: "http-review");
+
+//var notificationService = builder
+//    .AddProject<Projects.NotificationService>("notificationservice")
+//    .WithHttpEndpoint(port: 10008, name: "http-notification");
 
 //===========================================================================================================================================
 
@@ -37,10 +78,6 @@ discountService.WithReference(authService)
 reviewService.WithReference(authService)
              .WithReference(productService)
              .WithReference(orderService);
-
-
-
-
 
 //===========================================================================================================================================
 
