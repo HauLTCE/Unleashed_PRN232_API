a8b671a635db0c0047ed239cb3df737e945f6cf6
phanphucbinh92@gmail.com <phanphucbinh92@gmail.com>
2025-10-15 19:33:19 +0700

    discount

---
+++ Unleashed/DiscountService/Repositories/UserDiscountRepository.cs
@@ -0,0 +1,43 @@
+﻿using DiscountService.Models;
+using DiscountService.Repositories.Interfaces;
+
+namespace DiscountService.Repositories
+{
+    public class UserDiscountRepository : IUserDiscountRepository
+    {
+        public IQueryable<UserDiscount> All()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CreateAsync(UserDiscount entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(UserDiscount entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDiscount?> FindAsync((Guid, int) id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsAny((Guid, int) id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(UserDiscount entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
